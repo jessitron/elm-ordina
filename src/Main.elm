eaeb0c13fcca09674f328b3ff2ce8254108d0839
Jessica Kerr <jessitron@gmail.com>
2017-06-22 19:11:11 +0200

    commit it all right now

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,19 @@ module Main exposing (main)
 import Html exposing (Html)
 
 
+diagram =
+    "elmview.png"
+
+
 main : Html Never
 main =
-    Html.div [] []
+    Html.main_ []
+        [ Html.canvas
+            [ Html.Attributes.style
+                [ ( "backgroundImage"
+                  , "url(" + diagram + ")"
+                  )
+                ]
+            ]
+            []
+        ]
