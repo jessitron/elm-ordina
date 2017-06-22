22b9dc747202030249f77d48d7649543a3e3f04e
Jessica Kerr <jessitron@gmail.com>
2017-06-22 19:21:57 +0200

    commit it all right now

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,96 @@
 module Main exposing (main)
 
 import Html exposing (Html)
+import Html.Attributes
 
 
-diagram =
-    "elmview.png"
+-- MODEL
 
 
-main : Html Never
-main =
+type alias Model =
+    {}
+
+
+init : Model
+init =
+    {}
+
+
+
+-- MESSAGES
+
+
+type Msg
+    = NoOp
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
     Html.main_ []
         [ Html.canvas
             [ Html.Attributes.style
                 [ ( "backgroundImage"
-                  , "url(" + diagram + ")"
+                  , "url(" ++ diagram ++ ")"
                   )
                 ]
             ]
             []
+        , drawLabels [ { text = "CodeStar", x = 100, y = 10 } ]
         ]
+
+diagram =
+    "elmbp.png"
+
+
+
+
+
+type alias Label =
+    { text : String
+    , x : Int
+    , y : Int
+    }
+
+
+drawLabels : List Label -> Html Msg
+drawLabels labels =
+    let
+        formatLabel { text, y, x } =
+            Html.label
+                [ Html.Attributes.style
+                    [ ( "position", "absolute" )
+                    , ( "top", (toString y) ++ "px" )
+                    , ( "left", (toString x) ++ "px" )
+                    ]
+                ]
+                [ Html.text text ]
+    in
+        Html.div [] (List.map formatLabel labels)
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = init
+        , view = view
+        , update = update
+        }
